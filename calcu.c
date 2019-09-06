84c0ec548bc2de423f9342682dfd3e4aabc19405
saten <52410610+irielsurai@users.noreply.github.com>
2019-09-06 14:13:29 -0300

    Add files via upload

--- calcu.c
+++ calcu.c
@@ -3,9 +3,10 @@
 int calcular()
 {
 
-    float x;
-    float y;
+    float x;    // primera variable
+    float y;    // segunda variable
 
+    // objetos que van a guardar togas las respuestas:
     float rtaSuma;
     float rtaResta;
     float rtaProducto;
@@ -13,73 +14,95 @@ int calcular()
     float rtaFactorialX;
     float rtaFactorialY;
 
+    int flag;
+    flag = 0;
+    printf("Bienvenido a la calculadora!");
 
-    //1 - PIDE EL PRIMER NUMERO
-    printf("Por favor ingrese el primer número:");
-    scanf("%f", &x);
-
-    // 2 - PIDE EL SEGUNDO NUMERO
-    printf ("Por favor ingrese el segundo número:");
-    scanf("%f", &y);
-    // 3 - REALIZA LAS OPERACIONES
-    printf("\n\n\t A continuacion se presentan las operaciones \n \t de suma, resta, multiplicacion, division y \n \t factorial, para los valores %f y %f: \n\n", x,y);
-    rtaSuma = sumar (x, y);
-    rtaResta = restar (x, y);
-    rtaProducto = multiplicar (x, y);
-    if (y == 0)
+    while (flag ==0)
     {
-        rtaCociente = 0;
+        int menu;
+        printf ("\n \n  Elija la opcion del menu que desea realizar:\n  MENU:\n  1 - Ingresar el primer número\n  2 - Ingresar el segundo número \n  3 - Realizar las operaciones \n  4 - Mostrar los resultados \n  5 - Salir \n \n  ");
+        scanf("%d", &menu);
+        while (menu > 5 || menu <1) // Validacion del valor del menu.
+        {
+            printf ("\n No es una opcion valida de menu. \n Por favor indique un numero entero entre 1 a 5: \n  MENU:\n  1 - Ingresar el primer número\n  2 - Ingresar el segundo número \n  3 - Realizar las operaciones \n  4 - Mostrar los resultados \n  5 - Salir \n \n  ");
+            scanf("%d", &menu);
+        }
+        switch (menu)
+        {
+            case 1: //1 - PIDE EL PRIMER NUMERO
+                printf("Por favor ingrese el primer numero:");
+                scanf("%f", &x);
+                break;
+            case 2: // 2 - PIDE EL SEGUNDO NUMERO
+                printf ("Por favor ingrese el segundo numero:");
+                scanf("%f", &y);
+                break;
+            case 3: // 3 - REALIZA LAS OPERACIONES
+                printf("\n\n\t Se han realizado las operaciones  de suma, \n \t resta, multiplicacion, division y factorial, \n \t para los valores %.2f y %.2f: satisfactoriamente. \n\n", x,y);
+                rtaSuma = sumar (x, y);
+                rtaResta = restar (x, y);
+                rtaProducto = multiplicar (x, y);
+                if (y == 0)     //validacion de la division
+                {
+                    rtaCociente = 0;
+                }
+                else
+                {
+                    rtaCociente = dividir (x, y);
+                }
+                if (x>=0 && x<13) //validacion de los factoriales
+                {
+                    rtaFactorialX = factoriar (x);
+                }
+                    else
+                {
+                    rtaFactorialX = -1;
+                }
+                if (y>=0 && y<13)
+                {
+                    rtaFactorialY = factoriar (y);
+                }
+                    else
+                {
+                    rtaFactorialY = -1;
+                }
+                break;
+            case 4: // 4 - MUESTRA LOS RESULTADOS
+                printf("\n \n   RESULTADOS:");
+                printf ("\n   El resultado de %.2f + %.2f es: %.2f \n", x, y, rtaSuma);
+                printf ("   El resultado de %.2f - %.2f es: %.2f \n", x, y, rtaResta);
+                printf ("   El resultado de %.2f * %.2f es: %.2f \n", x, y, rtaProducto);
+                if (rtaCociente==0)
+                {
+                    printf ("   Lo lamentamos. No se puede dividir por cero \n");
+                    }
+                else
+                {
+                    printf ("   El resultado de %.2f / %.2f es: %.2f \n", x, y, rtaCociente);
+                }
+                if (rtaFactorialX == -1)
+                {
+                    printf ("   El factorial de %.2f no puede calcularse.", x);
+                }
+                else
+                {
+                    printf ("   El factorial de %.2f es: %.2f", x,rtaFactorialX);
+                }
+                if (rtaFactorialY == -1)
+                {
+                    printf (" El factorial de %.2f no puede calcularse.", y);
+                }
+                else
+                {
+                    printf (" El factorial de %.2f es: %.2f", y,rtaFactorialY);
+                }
+                break;
+            case 5: // SALE
+                printf("\n \n \n \t Gracias por usar esta bella calculadora. \n \t Pulse una tecla para salir. \n \n");
+                flag = 1; //El cambio de bandera hace que termine el while, y con él la calculadora
+        }
     }
-    else
-    {
-        rtaCociente = dividir (x, y);
-    }
-    if (x>=0 && x<13)
-    {
-        rtaFactorialX = factoriar (x);
-    }
-        else
-    {
-        rtaFactorialX = -1;
-    }
-    if (y>=0 && y<13)
-    {
-        rtaFactorialY = factoriar (y);
-    }
-        else
-    {
-        rtaFactorialY = -1;
-    }
-    // 4 - MUESTRA LOS RESULTADOS
-    printf ("   El resulado de %.2f + %.2f es: %.2f \n", x, y, rtaSuma);
-    printf ("   El resulado de %.2f - %.2f es: %.2f \n", x, y, rtaResta);
-    printf ("   El resulado de %.2f * %.2f es: %.2f \n", x, y, rtaProducto);
-    if (rtaCociente==0)
-    {
-        printf ("   No se puede dividir por cero \n");
-    }
-    else
-    {
-        printf ("   El resulado de %.2f / %.2f es: %.2f \n", x, y, rtaCociente);
-    }
-    if (rtaFactorialX == -1)
-    {
-        printf ("   El factorial de %.2f no puede calcularse.", x);
-    }
-    else
-    {
-        printf ("   El factorial de %.2f es: %.2f", x,rtaFactorialX);
-    }
-    if (rtaFactorialY == -1)
-    {
-        printf (" El factorial de %.2f no puede calcularse.", y);
-    }
-    else
-    {
-        printf (" El factorial de %.2f es: %.2f", y,rtaFactorialY);
-    }
-
-    printf("\n \n \n \t Pulse una tecla para salir. \n \n");
     return 0;
 }
 
